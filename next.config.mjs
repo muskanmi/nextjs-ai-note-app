816e1636628475920e232ca2bd5eeb2f87280a9a
Muskan Mittal <mittalmuskan758@gmail.com>
2024-02-21 19:48:15 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          hostname: "img.clerk.com",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
---
+++ src/app/notes/NavBar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddNoteDialog from "@/components/AddNoteDialog";
+import ThemeToggleButton from "@/components/ThemeToggleButton";
+import {dark} from "@clerk/themes"
+import { useTheme } from "next-themes";
+import AIChatButton from "@/components/AIChatButton";
+
+export default function NavBar() {
+  const {theme} = useTheme();
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+  return (
+    <>
+      <div className="p-4 shadow">
+        <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
+          <Link href="/notes" className="flex items-center gap-1">
+            <Image src={logo} alt="FlowBrain Logo" width={40} height={40} />
+            <span className="font-bold">FlowBrain</span>
+          </Link>
+          <div className="flex items-center gap-2">
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                baseTheme: (theme === "dark" ? dark : undefined),
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
+            <ThemeToggleButton />
+            <Button onClick={() => setShowAddNoteDialog(true)}>
+              <Plus size={20} className="mr-2" />
+              Add Note
+            </Button>
+            <AIChatButton />
+          </div>
+        </div>
+      </div>
+
+      { showAddNoteDialog && <AddNoteDialog open={showAddNoteDialog} setOpen={setShowAddNoteDialog} /> }
+    </>
+  );
+}
